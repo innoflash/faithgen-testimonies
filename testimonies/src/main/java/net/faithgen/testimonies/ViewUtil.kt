f7df30a175fb2a974998dbcdde320836eed1d6f3
innoflash <innocentmazando@gmail.com>
2020-02-19 17:42:43 +0200

    readied swiper and recylerview

--- testimonies/src/main/java/net/faithgen/testimonies/ViewUtil.kt
+++ testimonies/src/main/java/net/faithgen/testimonies/ViewUtil.kt
@@ -1,12 +1,17 @@
 package net.faithgen.testimonies
 
 import android.content.Context
+import android.view.Gravity
 import android.view.View
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import net.innoflash.iosview.recyclerview.RecyclerTouchListener
+import net.innoflash.iosview.recyclerview.RecyclerViewClickListener
 import net.innoflash.iosview.swipelib.SwipeRefreshLayout
 
-class ViewUtil(val context: Context, private val view: View) {
+class ViewUtil(val context: Context, private val view: View) : RecyclerViewClickListener,
+    SwipeRefreshLayout.OnRefreshListener {
 
     private val noTestimonies: TextView by lazy {
         view.findViewById<TextView>(R.id.noTestimonies)
@@ -17,5 +22,26 @@ class ViewUtil(val context: Context, private val view: View) {
 
     private val testimoniesView: RecyclerView by lazy {
         view.findViewById<RecyclerView>(R.id.testimoniesView)
+    }
+
+    fun initViewsCallbacks(){
+        testimoniesView.layoutManager = LinearLayoutManager(context)
+        testimoniesView.addOnItemTouchListener(RecyclerTouchListener(context, testimoniesView, this))
+
+        swipeRefresh.setOnRefreshListener(this)
+        swipeRefresh.setRefreshDrawableStyle(SwipeRefreshLayout.ARROW)
+        swipeRefresh.setPullPosition(Gravity.BOTTOM)
+    }
+
+    override fun onClick(view: View?, position: Int) {
+
+    }
+
+    override fun onLongClick(view: View?, position: Int) {
+        //do nothing
+    }
+
+    override fun onRefresh() {
+
     }
 }
